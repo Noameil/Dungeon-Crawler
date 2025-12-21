521a17a9b3c977749a7a398ec10c93e5403ab80a
noameil <noam.eilat@gmail.com>
2025-12-21 14:20:40 +0200

    List done

---
+++ List.hpp
@@ -0,0 +1,205 @@
+// Noam Eilat : 322713025
+// Amit Lachmann : 207448267
+
+#include <stdexcept>
+#include <iostream>
+
+#pragma once
+template <class T>
+class List
+{
+public:
+    typedef struct Node_s
+    {
+        T data;
+        struct Node_s *prev;
+        struct Node_s *next;
+    } Node;
+    Node *head;
+    Node *tail;
+
+    List()
+    {
+        try
+        {
+            this->head = createNode(nullptr);
+            this->tail = this->head;
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+    }
+    ~List()
+    {
+        Node *curr = this->head;
+        Node *temp = nullptr;
+        while (curr && curr != this->tail)
+        {
+            temp = curr->next;
+            delete curr;
+            curr = temp;
+        }
+        delete temp;
+    }
+
+    Node *createNode(T data);
+    void insertEnd(T data);
+    void insertStart(T data);
+    void insertAfterNode(T currData, T newData);
+    void remove(T data);
+    Node *search(T data);
+    bool isEmpty();
+    int size();
+    void printList();
+};
+
+template <class T>
+inline typename List<T>::Node *List<T>::createNode(T data)
+{
+    Node *newNode = new Node{data, nullptr, nullptr};
+    if (!newNode)
+        throw std::runtime_error("Error : Out of Memory List::createNode");
+
+    return newNode;
+}
+
+template <class T>
+inline void List<T>::insertEnd(T data)
+{
+    if (!this->tail->data)
+    {
+        this->tail->data = data;
+        return;
+    }
+    Node *newNode = createNode(data);
+    if (newNode)
+    {
+        this->tail->next = newNode;
+        newNode->prev = this->tail;
+        this->tail = this->tail->next;
+    }
+}
+
+template <class T>
+inline void List<T>::insertStart(T data)
+{
+    if (!this->head->data)
+    {
+        this->head->data = data;
+        return;
+    }
+    Node *newNode = createNode(data);
+    if (newNode)
+    {
+        this->head->prev = newNode;
+        newNode->next = this->head;
+        this->head = this->head->prev;
+    }
+}
+
+template <class T>
+inline void List<T>::insertAfterNode(T currData, T newData)
+{
+    Node *currNode = search(currData);
+    if (!currNode)
+        return;
+    Node *newNode = createNode(newData);
+    if (newNode)
+    {
+        if (currNode == this->tail)
+        {
+            this->tail->next = newNode;
+            newNode->prev = this->tail;
+            this->tail = this->tail->next;
+            return;
+        }
+        newNode->next = currNode->next;
+        currNode->next = newNode;
+        newNode->prev = currNode;
+        newNode->next->prev = newNode;
+    }
+}
+
+template <class T>
+inline void List<T>::remove(T data)
+{
+    if (this->head == this->tail)
+    {
+        if (this->head->data == data)
+        {
+            delete this->head;
+            this->head = nullptr;
+            this->head = this->tail;
+        }
+    }
+    if (this->head->data == data)
+    {
+        Node *temp = this->head;
+        this->head = this->head->next;
+        this->head->prev = nullptr;
+        delete temp;
+    }
+    if (this->tail->data == data)
+    {
+        Node *temp = this->tail;
+        this->tail = this->tail->prev;
+        this->tail->next = nullptr;
+        delete temp;
+    }
+    Node *curr = this->head;
+    while (curr)
+    {
+        if (curr->data == data)
+        {
+            Node *temp = curr;
+            curr->prev->next = curr->next;
+            temp->next->prev = temp->prev;
+            delete temp;
+            break;
+        }
+        curr = curr->next;
+    }
+}
+
+template <class T>
+inline typename List<T>::Node *List<T>::search(T data)
+{
+    Node *curr = this->head;
+    while (curr)
+    {
+        if (curr->data == data)
+            return curr;
+    }
+    return nullptr;
+}
+
+template <class T>
+inline bool List<T>::isEmpty()
+{
+    return (this->head->data) ? false : true;
+}
+
+template <class T>
+inline int List<T>::size()
+{
+    int sizeCount = 0;
+    Node *curr = this->head;
+    while (curr)
+    {
+        sizeCount++;
+        curr = curr->next;
+    }
+    return sizeCount;
+}
+
+template <class T>
+inline void List<T>::printList()
+{
+    Node *curr = this->head;
+    while (curr)
+    {
+        std::cout << *curr->data << std::endl;
+        curr = curr->next;
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,4 +1,12 @@
-int main() {
+// Noam Eilat : 322713025
+// Amit Lachmann : 207448267
+
+#include <iostream>
+#include "List.hpp"
+#include "Item.hpp"
+
+int main()
+{
 
     Game game;
     game.loadFromFile("input.txt");
@@ -7,6 +15,22 @@ int main() {
     game.executeCommands();
 
     game.outputFinalState("output.txt");
+
+    // List<int *> list;
+    // int x = 1;
+    // int y = 2;
+    // int z = 3;
+    // int k = 4;
+    // // std::cout << list.isEmpty() << std::endl;
+    // list.insertEnd(&x);
+    // list.insertEnd(&y);
+    // // list.insertAfterNode(&x, &z);
+    // // list.insertStart(&k);
+    // // std::cout << list.isEmpty() << std::endl;
+    // // std::cout << list.size() << std::endl;
+    // list.printList();
+    // list.remove(&x);
+    // list.printList();
 
     return 0;
 }
